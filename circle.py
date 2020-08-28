b44371960500b2091f6b700e2e513b5bb97665c7
Tyler Moore <tylermoore@Tylers-MacBook-Pro-5.local>
2020-08-28 11:36:11 -0700

    commit

--- circle.py
+++ circle.py
@@ -1,17 +1,21 @@
 from graphics import *
+import random
 
 def main():
-    win = GraphWin("My Circle", 100, 100)
-    win.setBackground("blue")
-    c = Circle(Point(50,50), 10)
-    c.setFill("red")
+    win = GraphWin("My Circle", 500, 500)
+    win.setBackground(color_rgb(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
+    mousepos = win.getMouse()
+    c = Circle(Point(mousepos.x,mousepos.y), random.randint(0, 100))
+    c.setFill(color_rgb(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
     c.draw(win)
-    print(win.getMouse()) # Pause to view result
-    win.close()    # Close window when done
-
+    win.getMouse() # Pause to view result
+    
 main()
 
-# 1 order book, 2 python rand int
-# make window 500 x 500 
+# 1 order book, 2 python rand int done
+# make window 500 x 500 - done
 # get mouse position first, draw circle on mouse position, draw in random color.
 # git it to connect to git
+
+
+# QUESTION: how do I know I can use x or y? It was just a guess.
